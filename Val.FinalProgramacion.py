0ecebc5a39b051014f0b15f5d604ab7ee35be5dd
fcpiero6 <106614124+fcpiero6@users.noreply.github.com>
2022-08-14 21:06:29 -0300

    Add files via upload

---
+++ Val.FinalProgramacion.py
@@ -0,0 +1,11 @@
+#Ejercicio N°10 Python 
+#Crear una lista denominada “Clientes” con los nombres de los diferentes dueños de perros y ordenarlos alfabeticamente.
+#Nombres: Juan, Mario, Ariel, Josefina, Marianella
+
+Clientes = ["Juan", "Mario", "Ariel", "Josefina", "Marianella"]
+Clientes.sort()
+print(Clientes)
+Nombres=input("Ingrese el nombre de un cliente:")
+print(Nombres)
+
+
